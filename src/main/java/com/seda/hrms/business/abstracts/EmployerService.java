6e1b7b7e135a825518781d9c71de69cb3caf3bbb
Seda <sedasugur98@hotmail.com>
2021-11-18 00:06:41 +0300

    add EmployerService file

---
+++ src/main/java/com/seda/hrms/business/abstracts/EmployerService.java
@@ -0,0 +1,10 @@
+package com.seda.hrms.business.abstracts;
+
+import java.util.List;
+
+import com.seda.hrms.core.utilities.results.DataResult;
+import com.seda.hrms.entities.concretes.Employer;
+
+public interface EmployerService {
+	public DataResult<List<Employer>> getAll();
+}
